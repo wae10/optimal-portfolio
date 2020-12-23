7b81902233525ee54c075539b8902cf62d8ef1b7
wae10 <wae10@georgetown.edu>
2020-12-23 13:22:12 -0800

    favicon fix

---
+++ web_app/static/favicon.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask, send_from_directory, render_template
+
+app = Flask(__name__)
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
+    
