246c5e9256a9ad2c37f281df1322bd0ff682d990
wae10 <wae10@georgetown.edu>
2020-12-23 12:23:48 -0800

    favicon test route change

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -51,10 +51,9 @@ plt.switch_backend('Agg')
 import base64
 
 
-# @home_routes.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(home_routes.root_path, 'static'), 'favicon.ico', type="image/x-icon")
-
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
 
 @home_routes.route("/plot/done", methods=["POST"])
 def plot():
