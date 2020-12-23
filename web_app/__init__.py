a66554bf281a1934bb26ad49fde7e9226f5e34cf
wae10 <wae10@georgetown.edu>
2020-12-23 14:30:12 -0800

    frontend

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -9,11 +9,8 @@ from web_app.routes.home_routes import home_routes
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
-
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = SECRET_KEY
 
     app.register_blueprint(home_routes)
 
--- web_app/static/favicon.py
+++ web_app/static/favicon.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask, send_from_directory, render_template
-
+from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 @app.route("/favicon.ico")
