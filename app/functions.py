933fb991224ff5e1e27c9b012c3faa9831981b52
wae10 <wae10@georgetown.edu>
2020-12-23 16:37:01 -0800

    fix terminal functionality

--- app/functions.py
+++ app/functions.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 
 from dotenv import load_dotenv
 import os
-from app import APP_ENV
+# from app import APP_ENV
 
+from __init__ import APP_ENV
 
 def _plot_io(**kwargs):
     """
@@ -86,4 +87,66 @@ def plot_efficient_frontier(cla, start, end, points=100, show_assets=True, **kwa
 
 
     # _plot_io(**kwargs)
+    return fig, ax
+
+
+def plot_efficient_frontier1(cla, start, end, points=100, show_assets=True, **kwargs):
+    """
+    Plot the efficient frontier based on a CLA object
+
+    :param points: number of points to plot, defaults to 100
+    :type points: int, optional
+    :param show_assets: whether we should plot the asset risks/returns also, defaults to True
+    :type show_assets: bool, optional
+    :param filename: name of the file to save to, defaults to None (doesn't save)
+    :type filename: str, optional
+    :param showfig: whether to plt.show() the figure, defaults to True
+    :type showfig: bool, optional
+    :return: matplotlib axis
+    :rtype: matplotlib.axes object
+    """
+    if cla.weights is None:
+        cla.max_sharpe()
+    optimal_ret, optimal_risk, _ = cla.portfolio_performance()
+
+    if cla.frontier_values is None:
+        cla.efficient_frontier(points=points)
+
+    mus, sigmas, _ = cla.frontier_values
+
+    fig, ax = plt.subplots()
+    ax.plot(sigmas, mus, label="Efficient Frontier", linewidth=3)
+
+    if show_assets:
+        ax.scatter(
+            np.sqrt(np.diag(cla.cov_matrix)),
+            cla.expected_returns,
+            s=120,
+            color="k",
+            label="Assets",
+        )
+        #NEW...
+        for i in range(len(cla.tickers)):
+            plt.text(np.sqrt(np.diag(cla.cov_matrix))[i] + 0.005,cla.expected_returns[i] + 0.005, cla.tickers[i], fontsize=15, ha='left')
+
+
+
+
+    ax.scatter(optimal_risk, optimal_ret, marker="X", s=200, color="r", label="Optimal (Max Sharpe Ratio)")
+    ax.legend()
+    ax.set_xlabel("Standard Deviation", fontsize=20)
+    ax.set_ylabel("Expected Return", fontsize=20) #expected return, based on annualized return calculated
+
+    title = "Efficient Frontier \n(based on rtns from " + start + " to " + end + ")"
+
+    plt.title(label=title,fontsize=27,pad=20)
+
+    plt.legend(prop={"size":13})
+
+    plt.xticks(fontsize= 13)
+
+    plt.yticks(fontsize= 13)
+
+
+    _plot_io(**kwargs)
     return fig, ax
--- app/script.py
+++ app/script.py
@@ -11,21 +11,24 @@ from pypfopt.cla import CLA
 from pypfopt.plotting import plot_weights
 from matplotlib.ticker import FuncFormatter
 from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
-from app.functions import plot_efficient_frontier
+from functions import plot_efficient_frontier, plot_efficient_frontier1
 from collections import namedtuple
 
 #NEW
 from dotenv import load_dotenv
 import os
-from app import APP_ENV
+# from app import APP_ENV
 
-def optimal_shares(tickers, start, end):
+from __init__ import APP_ENV
+
+def optimal_shares(tickers, start, end, amount):
     """Returns optimal llocation to individual assets to maximize sharpe ratio of portfolio
 
     Args:
         tickers (list): list of desired stock tickers
         start (string): starting date, yyyy-mm-dd
         end (string): ending date, yyyy-mm-dd
+        amount (double): amount invested
     """
 
     if start is None or start == " ":
@@ -64,7 +67,7 @@ def optimal_shares(tickers, start, end):
     latest_prices = get_latest_prices(df_stocks)
     # Allocate Portfolio Value in $ as required to show number of shares/stocks to buy, also bounds for shorting will affect allocation
     #Min Volatility Portfolio Allocation $10000
-    allocation, leftover = DiscreteAllocation(sharpe_pwt, latest_prices, total_portfolio_value=10000).lp_portfolio()
+    allocation, leftover = DiscreteAllocation(sharpe_pwt, latest_prices, total_portfolio_value=amount).lp_portfolio()
     print("Optimal shares to buy:", allocation)
     print("Leftover Fund value in $ after building minimum volatility portfolio is ${:.2f}".format(leftover))
 
@@ -221,12 +224,13 @@ def menu():
         print_menu()    ## Displays menu
         choice = eval(input("Enter your choice [1-5]: "))
         
-        if choice==1:     
-            optimal_shares(tickers, start, end)
+        if choice==1:
+            amount = eval(input("Enter amount you want to invest (format as number): "))     
+            optimal_shares(tickers, start, end, amount)
 
         elif choice==2:
             cla, start, end = get_cla(tickers,start,end)
-            plot_efficient_frontier(cla, start, end, points=100, show_assets=True)
+            plot_efficient_frontier1(cla, start, end, points=100, show_assets=True)
 
         elif choice==3:
             graph_closes(tickers,start,end)
